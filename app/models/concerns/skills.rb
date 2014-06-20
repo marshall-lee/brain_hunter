8bc9004e8b68e8a9879ca850ad4a8f9e81b7517a
Vladimir Mitnick <hashtable@yandex.ru>
2014-06-20 09:41:41 +0400

    refactor Skills concern

--- app/models/concerns/skills.rb
+++ app/models/concerns/skills.rb
@@ -3,13 +3,21 @@ module Skills
 
   included do |klass|
     acts_as_taggable_on :skills
-    
-    def self.all_skills
-      tags_on(:skills).pluck(:name)
+
+    def self.all_skill_tags
+      tags_on(:skills)
     end
 
-    def self.all_skills_like(query)
-      tags_on(:skills).where("name like :query", query: query).pluck(:name)
+    def self.all_skill_tags_like(query)
+      all_skill_tags.where("name like :query", query: query)
+    end
+
+    def self.all_skills
+      all_skill_tags.pluck(:name)
+    end
+
+    def self.all_skills_like(query, options = {})
+      all_skill_tags_like(query).pluck(:name)
     end
   end
 end
