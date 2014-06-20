d8334d00e334faf058283e2beaf4d4d0017db7d6
Vladimir Mitnick <hashtable@yandex.ru>
2014-06-20 06:04:39 +0400

    Implement :actual scope for Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ActiveRecord::Base
   validates_presence_of :title
+
+  scope :actual, -> { where(':now < expires_at', now: Time.now) }
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Job, :type => :model do
   it { should validate_presence_of :title }
+
+  describe "actual scope" do
+    it "presents" do
+      expect(Job).to respond_to :actual
+    end
+
+    it "doesn't return expired jobs" do
+      actual_job = FactoryGirl.create(:job)
+      expired_job = FactoryGirl.create(:job, expires_at: Time.now - 1.day)
+      expect(Job.all).to be_include(actual_job).and be_include(expired_job)
+      expect(Job.actual).to be_include(actual_job)
+      expect(Job.actual).not_to be_include(expired_job)
+    end
+  end
 end
