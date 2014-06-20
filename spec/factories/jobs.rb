b3add9e16e29f9da5b05250fea01dbf47f54aada
Vladimir Mitnick <hashtable@yandex.ru>
2014-06-20 05:52:09 +0400

    Write Job factory

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,10 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :job do
-    title "MyString"
-    expires_at "2014-06-20"
-    salary 1
-    email "MyString"
+    sequence(:title) { |n| "Sample job ##{n}" }
+    expires_at { Time.now + 5.days }
+    salary 10000
+    email "hr@example.com"
   end
 end
